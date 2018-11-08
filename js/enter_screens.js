f07dd7d55313953d9004e3745016e9b64e8e1edb
namisumida <sumida.nami@gmail.com>
2018-11-08 14:07:02 -0500

    Final edits
    Added methods text, gray background, added scrollama entrance to screen11

--- js/enter_screens.js
+++ js/enter_screens.js
@@ -716,407 +716,171 @@ function enter_screen8() {
 				 });
 
 } // end enter_screen8
-/*
-function enter_screen9() {
-	// transition from screen8
-	screen6.selectAll("text")
-				 .transition()
-				 .duration(400)
-				 .style("fill", "none");
-	screen6.selectAll(".intro_text") // But not the intro text - easier to remove off of this than do each one individually in previous
-				 .selectAll("tspan")
-				 .style("fill", "black");
-	svg.selectAll(".description") // label lines
-				 .transition()
-				 .duration(400)
-				 .style("stroke", "none");
-	svg.selectAll(".description") // label text
-		 .selectAll("tspan")
-				 .transition()
-				 .duration(400)
-				 .style("fill", "none");
-	screen3.selectAll(".grid_rect")
-    		 .transition()
-    		 .duration(600)
-    		 .ease(d3.easeLinear)
-    		 .attr("y", -500);
-	screen8.selectAll("rect")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", -500);
-	screen3.selectAll(".chyron_back")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", -500);
-	screen8.selectAll(".count_text")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", -500);
-	exit_screen8();
-	screen7.selectAll(".count_text")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", -500);
 
-	// Transition out of screen 10
-	exit_screen10();
-
-	// Add in intro text
-	screen9.select("#intro9_1")
-				 .selectAll("tspan")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", 12);
-	screen9.select("#intro9_2")
-				 .selectAll("tspan")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", 60);
-
-	// Move brand text
-	screen3.selectAll(".brand_text")
-          .attr("y", -500)
-    			.transition()
-    			.duration(600)
-    			.ease(d3.easeLinear)
-    			.attr("y", pos_brandtext-30)
-          .attr("x", function(d) {
-            if (d.network=="msnbc") {
- 						 return margin.left_chyron2+w_chyron/2;
- 					 }
- 					 else if (d.network=="cnn") {
- 						 return margin.left_chyron2*2+(w_chyron/2)*3;
- 					 }
- 					 else { return margin.left_chyron2*3+(w_chyron/2)*5; }
-          });
-
-	// Move chyron rects
-	screen9.selectAll(".chyron_rect")
-         .attr("y", -500)
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", pos_chyron-20);
-
-	screen9.selectAll(".chyron_text")
-				 .selectAll("tspan")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", pos_chyron+20);
-
-} // end enter_screen9
-
-function enter_screen10() {
-	// Transition out of screen 9
-	screen9.selectAll("text")
-				 .selectAll("tspan")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", -500);
-
-	// Transition out of screen 11
-	// Move brand text
-	screen3.selectAll(".brand_text")
-          .attr("y", -500)
-    			.transition()
-    			.duration(600)
-    			.ease(d3.easeLinear)
-    			.attr("y", pos_brandtext)
-          .attr("x", function(d) {
-            if (d.network=="msnbc") {
- 						 return margin.left_chyron2+w_chyron/2;
- 					 }
- 					 else if (d.network=="cnn") {
- 						 return margin.left_chyron2*2+(w_chyron/2)*3;
- 					 }
- 					 else { return margin.left_chyron2*3+(w_chyron/2)*5; }
-          });
-	// Move chyron rects
-	screen9.selectAll(".chyron_rect")
-         .attr("y", -500)
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", pos_chyron+10);
-	// Move out circles
-	exit_screen11();
-	screen11.selectAll(".description")
-					.selectAll("tspan")
-					.transition()
-					.duration(400)
-					.style("fill", "none");
-	screen11.selectAll("line")
-					.transition()
-					.duration(400)
-					.style("stroke", "none");
-
-	// Move in intro text
-	screen10.select("#intro10_1")
-					.selectAll("tspan")
-					.transition()
-					.duration(600)
-					.ease(d3.easeLinear)
-					.attr("y", 12);
-  screen10.select("#intro10_2")
-					.selectAll("tspan")
-					.transition()
-					.duration(600)
-					.ease(d3.easeLinear)
-					.attr("y", 100);
-
-	// Move in new chyron text
-	screen10.selectAll(".chyron_text")
-				 .selectAll("tspan")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", function(d) {
-					 if (d.network=="msnbc") {
-						 return pos_chyron+30;
-					 }
-					 else { return pos_chyron+35; }
-				 });
-} // end enter_screen10
-
+// Transitions for screen 11
 function enter_screen11() {
-  // Transition from screen_tool
-  screen11.transition()
+  // Transition for Ford circles
+  screen11.selectAll(".grid_circle")
+          .filter(function(d) {
+            return d.chyron.includes("FORD:");
+          })
+          .transition()
+          .delay(500)
           .duration(600)
-          .ease(d3.easeLinear)
-          .attr("transform", "translate(0,0)");
-  screen_tool.transition()
+          .style("opacity", 1)
+          .style("fill", function(d) {
+            if (d.network=="msnbc") {
+                return light_yellow;
+              }
+              else if (d.network=="cnn") {
+               return light_red;
+              }
+              else { return light_blue; }
+          })
+          .style("stroke", function(d) {
+            if (d.network=="msnbc") {
+              return dark_yellow;
+            }
+            else if (d.network=="cnn") {
+              return dark_red;
+            }
+            else { return dark_blue; }
+          })
+          .style("stroke-width", 2);
+
+  // Description/labels
+  screen11.select("#ford_line")
+          .transition()
+          .delay(1000)
           .duration(600)
-          .ease(d3.easeLinear)
-          .attr("transform", "translate(0,-800)");
-  document.getElementById("slider").style.visibility="hidden"; // hide slider for now
+          .style("stroke", "gray");
+  screen11.select("#ford_descrip")
+          .selectAll("tspan")
+          .transition()
+          .delay(1000)
+          .duration(600)
+          .style("fill", "gray");
 
+  // Transition for Kav circles
+  screen11.selectAll(".grid_circle")
+          .filter(function(d) {
+            return d.chyron.includes("KAVANAUGH:");
+          })
+          .transition()
+          .delay(1500)
+          .duration(600)
+          .style("opacity", 1)
+          .style("fill", function(d) {
+            if (d.network=="msnbc") {
+              return yellow;
+            }
+            else if (d.network=="cnn") {
+              return red;
+            }
+            else { return blue; }
+          });
 
-	// Transition from screen 10
-	exit_screen10();
-	screen9.selectAll(".chyron_rect")
-				 .transition()
-				 .duration(600)
-				 .ease(d3.easeLinear)
-				 .attr("y", -500);
+  // Description/labels
+  screen11.select("#kav_line")
+      .transition()
+      .delay(2000)
+      .duration(600)
+      .style("stroke", "gray");
+  screen11.select("#kav_descrip")
+          .selectAll("tspan")
+          .transition()
+          .delay(2000)
+          .duration(600)
+          .style("fill", "gray");
 
-	// Move brand text
-	screen3.selectAll(".brand_text")
-         .attr("y", -500)
-  			 .transition()
-  			 .duration(600)
-  			 .ease(d3.easeLinear)
-  			 .attr("y", pos_brandtext-40)
-         .attr("x", function(d) {
-           if (d.network=="msnbc") {
-             return margin.left_chyron+w_chyron/2;
-           }
-           else if (d.network=="cnn") {
-             return margin.left_chyron+margin.btwn_chyron+(w_chyron/2)*3;
-           }
-           else { return margin.left_chyron+margin.btwn_chyron*2+(w_chyron/2)*5; }
-         });
-
-	// Intro text
-	screen11.select("#intro11_1")
-					.selectAll("tspan")
-					.transition()
-					  .duration(600)
-					  .ease(d3.easeLinear)
-					  .attr("y", 12);
-	screen11.select("#intro11_2")
-					.selectAll("tspan")
-					.transition()
-					.duration(600)
-					.ease(d3.easeLinear)
-					.attr("y", 95);
-
-	// Grid circles
-	screen11.selectAll(".grid_circle")
-				  .transition()
-					.duration(600)
-					.ease(d3.easeLinear)
-					.attr("cy", function(d,i) {
-						if (d.network=="msnbc") {
-							return pos_gridstart+Math.floor(i / 10) % max_rows * sq_spacing + sq_spacing/2;
-						}
-						else if (d.network=="cnn") {
-							return pos_gridstart+Math.floor((i-103) / 10) % max_rows * sq_spacing + sq_spacing/2;
-						}
-						else { return pos_gridstart+Math.floor((i-173) / 10) % max_rows * sq_spacing + sq_spacing/2; }
-					})
-					.style("opacity", .25);
-
-	// Transition for Ford circles
-	screen11.selectAll(".grid_circle")
-					.filter(function(d) {
-						return d.chyron.includes("FORD:");
-					})
-					.transition()
-					.delay(1000)
-					.duration(600)
-					.style("opacity", 1)
-					.style("fill", function(d) {
-						if (d.network=="msnbc") {
-								return light_yellow;
-							}
-							else if (d.network=="cnn") {
-							 return light_red;
-							}
-							else { return light_blue; }
-					})
-					.style("stroke", function(d) {
-						if (d.network=="msnbc") {
-							return dark_yellow;
-						}
-						else if (d.network=="cnn") {
-							return dark_red;
-						}
-						else { return dark_blue; }
-					})
-					.style("stroke-width", 2);
-
-	// Description/labels
-	screen11.select("#ford_line")
-					.transition()
-					.delay(1500)
-					  .duration(600)
-					.style("stroke", "gray");
-	screen11.select("#ford_descrip")
-					.selectAll("tspan")
-					.transition()
-					.delay(1500)
-					.duration(600)
-					.style("fill", "gray");
-
-// Transition for Kav circles
-screen11.selectAll(".grid_circle")
-				.filter(function(d) {
-					return d.chyron.includes("KAVANAUGH:");
-				})
-				.transition()
-				  .delay(2000)
-				  .duration(600)
-				.style("opacity", 1)
-				.style("fill", function(d) {
-					if (d.network=="msnbc") {
-						return yellow;
-					}
-					else if (d.network=="cnn") {
-						return red;
-					}
-					else { return blue; }
-				});
-
-// Description/labels
-screen11.select("#kav_line")
-		.transition()
-		.delay(2500)
-		.duration(600)
-		.style("stroke", "gray");
-screen11.select("#kav_descrip")
-				.selectAll("tspan")
-				.transition()
-				.delay(2500)
-				.duration(600)
-				.style("fill", "gray");
-
-// Mouseover
-screen11.selectAll(".grid_circle")
-				.filter(function(d) {
-					return d.chyron.includes("FORD:") | d.chyron.includes("KAVANAUGH:");
-				})
-				.on("mouseover", function(d) {
-					var network = d.network;
-					screen11.append("rect")
-								 .attr("id", "chyron_rect")
-								 .attr("x", function() {
-                  if (network=="msnbc") {
-       							return margin.left_chyron;
-       						}
-       						else if (network=="cnn") {
-       							return margin.left_chyron+margin.btwn_chyron+w_chyron;
-       						}
-       						else { return margin.left_chyron+margin.btwn_chyron*2+w_chyron*2; }
-								 })
-								 .attr("y", function() {
-									 if (network=="msnbc") {
-										return pos_circlemouse;
-									 }
-									 else { return pos_circlemouse-80; }
-								 })
-								 .style("fill", function() {
-									 if (network=="msnbc") {
-										return yellow;
-									}
-									else if (network=="cnn") {
-										return red;
-									}
-									else { return blue; }
-								 })
-								 .attr("width", w_chyron)
-								 .attr("height", h_chyron)
-								 .style("opacity", 0.5);
-
-					 var chyron = d.chyron;
-					 screen11.append("text")
-									.attr("id", "chyron_text")
-									.text(chyron)
-									.attr("x", function() {
+  // Mouseover feature of grid circles
+  screen11.selectAll(".grid_circle")
+          .filter(function(d) {
+            return d.chyron.includes("FORD:") | d.chyron.includes("KAVANAUGH:");
+          })
+          .on("mouseover", function(d) {
+            var network = d.network;
+            screen11.append("rect")
+                    .attr("id", "chyron_rect")
+                   .attr("x", function() {
                     if (network=="msnbc") {
-        							return margin.left_chyron+w_chyron/2;
-        						}
-        						else if (network=="cnn") {
-        							return margin.left_chyron+margin.btwn_chyron+(w_chyron/2)*3;
-        						}
-        						else { return margin.left_chyron+margin.btwn_chyron*2+(w_chyron/2)*5; }
-									})
-									.attr("y", function() {
-									 labelWidth = this.getComputedTextLength();
-									 availWidth = w_chyron-10;
-									 lines = Math.ceil(labelWidth/availWidth);
-									 if (network=="msnbc") {
-										 if (lines <= 5) {
-											 return pos_circlemouse+30;
-										 }
-										 else {
-											 return pos_circlemouse+20;
-										 }
-									 }
-									 else {
-										 if (lines <= 5) {
-											 return pos_circlemouse-80+30;
-										 }
-										 else {
-											 return pos_circlemouse-80+20;
-										 }
-									 }
-									})
-									.style("text-anchor", "middle")
-									.style("font-size", function() {
-                    if (lines <= 4) {
-                      return 14;
+                      return margin.left_chyron - (sq_size-8);
                     }
-                    else if (lines <=5) {
-                      return 14 - Math.ceil(lines/4)*.5 + "px";
+                    else if (network=="cnn") {
+                      return margin.left_chyron+margin.btwn_chyron+w_chyron - (sq_size-8);
                     }
-                    else {
-                      return 14 - Math.ceil(lines/2)*.5 + "px";
+                    else { return margin.left_chyron+margin.btwn_chyron*2+w_chyron*2 - (sq_size-8); }
+                   })
+                   .attr("y", function() {
+                     if (network=="msnbc") {
+                      return pos_circlemouse;
+                     }
+                     else { return pos_circlemouse-80; }
+                   })
+                   .style("fill", function() {
+                     if (network=="msnbc") {
+                      return yellow;
                     }
-								 })
-									.style("font-weight", 400)
-									.call(wrap, w_chyron-10);
-				}) // end on mouseover
-				.on("mouseout", function() {
-					screen11.select("#chyron_rect").remove();
-					screen11.select("#chyron_text").remove();
-				});
+                    else if (network=="cnn") {
+                      return red;
+                    }
+                    else { return blue; }
+                   })
+                   .attr("width", w_chyron)
+                   .attr("height", h_chyron)
+                   .style("opacity", 0.5);
 
-} // end enter_screen11 */
+             var chyron = d.chyron;
+             screen11.append("text")
+                    .attr("id", "chyron_text")
+                    .text(chyron)
+                    .attr("x", function() {
+                      if (network=="msnbc") {
+                        return margin.left_chyron+w_chyron/2 - (sq_size-8);
+                      }
+                      else if (network=="cnn") {
+                        return margin.left_chyron+margin.btwn_chyron+(w_chyron/2)*3 - (sq_size-8);
+                      }
+                      else { return margin.left_chyron+margin.btwn_chyron*2+(w_chyron/2)*5 - (sq_size-8); }
+                    })
+                    .attr("y", function() {
+                     labelWidth = this.getComputedTextLength();
+                     availWidth = w_chyron-10;
+                     lines = Math.ceil(labelWidth/availWidth);
+                     if (network=="msnbc") {
+                       if (lines <= 5) {
+                         return pos_circlemouse+30;
+                       }
+                       else {
+                         return pos_circlemouse+20;
+                       }
+                     }
+                     else {
+                       if (lines <= 5) {
+                         return pos_circlemouse-80+30;
+                       }
+                       else {
+                         return pos_circlemouse-80+20;
+                       }
+                     }
+                    })
+                    .style("text-anchor", "middle")
+                    .style("font-size", function() {
+                      if (lines <= 4) {
+                        return 14;
+                      }
+                      else if (lines <=5) {
+                        return 14 - Math.ceil(lines/4)*.5 + "px";
+                      }
+                      else {
+                        return 14 - Math.ceil(lines/2)*.5 + "px";
+                      }
+                   })
+                    .style("font-weight", 400)
+                    .call(wrap, w_chyron-10);
+          }) // end on mouseover
+          .on("mouseout", function() {
+            screen11.select("#chyron_rect").remove();
+            screen11.select("#chyron_text").remove();
+          });
+}; // end enter function
